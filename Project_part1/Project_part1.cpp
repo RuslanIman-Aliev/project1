9857c2ec3805e0864d1d9db937fab8621829fb4d
Иман-Алиев Руслан <s0970802047@gmail.com>
2023-10-17 22:50:25 +0300

    Update input

--- Project_part1/Project_part1.cpp
+++ Project_part1/Project_part1.cpp
@@ -34,30 +34,32 @@ int function_operation(int number1, char math_operator, int number2) {
 
     return result;
 }
-
 void function_for_read_data() {
-    int number1, number2, result;
-    char math_operator;
+    int  result;
     string input;
 
-    do {
-        cout << "Enter an expression in the format 'int math_operator int' with space between numbers or 'q' for exit" << endl;
+    cout << "Enter an expression in the format 'int math_operator int' with space between numbers or 'q' for exit" << endl;
+
+    while (true) {
         getline(cin, input);
 
         if (input == "q") {
             break;
         }
 
-        try {
-            istringstream(input) >> number1 >> math_operator >> number2;
-            result = function_operation(number1, math_operator, number2);
-            cout << "Result: " << result << endl;
+        istringstream input_stream(input);
+        char math_operator;
+        int number2;
+
+        input_stream >> result;
+        while (input_stream >> math_operator >> number2) {
+            result = function_operation(result, math_operator, number2);
         }
-        catch (const exception& e) {
-            cout << "Error: " << e.what() << "\n";
-        }
-    } while (true);
+
+        cout << "Result: " << result << endl;
+    }
 }
+
 int main()
 {
     function_for_read_data();
