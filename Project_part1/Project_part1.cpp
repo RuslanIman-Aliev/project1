be7ddd9e052501ac330720879de22df4737463dd
Иман-Алиев Руслан <s0970802047@gmail.com>
2023-10-23 23:07:04 +0300

    Update priority

--- Project_part1/Project_part1.cpp
+++ Project_part1/Project_part1.cpp
@@ -1,67 +1,120 @@
 ﻿#include <iostream>
 #include <string>
-#include <sstream>
 #include <exception>
+#include <cctype>
 
 using namespace std;
 
-int function_operation(int number1, char math_operator, int number2) {
-    int result;
+double evaluate_expression(const string& expression, size_t& index) {
+    double result = 0;
+    char last_operator = '+';
+    bool number_found = false;
 
-    switch (math_operator) {
-    case '+':
-        result = number1 + number2;
-        break;
+    while (index < expression.length()) {
+        char current_char = expression[index];
 
-    case '-':
-        result = number1 - number2;
-        break;
-
-    case '*':
-        result = number1 * number2;
-        break;
-
-    case '/':
-        if (number2 == 0) {
-            throw runtime_error("Division by zero");
+        if (current_char == ' ') {
+            index++;
+            continue;
         }
-        result = number1 / number2;
-        break;
+        else if (isdigit(current_char)) {
+            double number = stod(expression.substr(index));
 
-    default:
-        throw invalid_argument("Invalid operator.");
+            while (index < expression.length() && (isdigit(expression[index]) || expression[index] == ' ')) {
+                index++;
+            }
+
+            while (index < expression.length() && (expression[index] == '*' || expression[index] == '/')) {
+                char op = expression[index];
+
+                index++;
+                if (index >= expression.length()) {
+                    throw invalid_argument("Missing number after operator");
+                }
+                double next_number = stod(expression.substr(index));
+
+                while (index < expression.length() && (isdigit(expression[index]) || expression[index] == ' ')) {
+                    index++;
+                }
+
+                if (op == '*') {
+                    number *= next_number;
+                }
+                else if (op == '/') {
+                    if (next_number == 0) {
+                        throw runtime_error("Division by zero");
+                    }
+                    number /= next_number;
+                }
+            }
+
+            if (last_operator == '+') {
+                result += number;
+            }
+            else if (last_operator == '-') {
+                result -= number;
+            }
+            number_found = true;
+        }
+        else if (current_char == '+' || current_char == '-') {
+            last_operator = current_char;
+            index++;
+        }
+        else if (current_char == '(') {
+            index++;
+
+            double sub_result = evaluate_expression(expression, index);
+
+            if (last_operator == '+') {
+                result += sub_result;
+            }
+            else if (last_operator == '-') {
+                result -= sub_result;
+            }
+
+            number_found = true;
+        }
+        else if (current_char == ')') {
+            index++;
+            break;
+        }
+        else {
+            throw invalid_argument("Invalid character in expression");
+        }
+    }
+
+    if (!number_found) {
+        throw invalid_argument("No number found");
     }
 
     return result;
 }
+
 void function_for_read_data() {
-    int  result;
+
     string input;
 
-    cout << "Enter an expression in the format 'int math_operator int' with space between numbers or 'q' for exit" << endl;
-
-    while (true) {
+    do {
+        cout << "Enter an expression  or 'q' to exit: ";
         getline(cin, input);
 
         if (input == "q") {
             break;
         }
 
-        istringstream input_stream(input);
-        char math_operator;
-        int number2;
+        try {
+            size_t index = 0;
+            double result = evaluate_expression(input, index);
 
-        input_stream >> result;
-        while (input_stream >> math_operator >> number2) {
-            result = function_operation(result, math_operator, number2);
+            cout << "Result: " << result << endl;
         }
-
-        cout << "Result: " << result << endl;
-    }
+        catch (const exception& e) {
+            cout << "Error: " << e.what() << "\n";
+        }
+    } while (true);
 }
 
-int main()
-{
+int main() {
     function_for_read_data();
     return 0;
 }
