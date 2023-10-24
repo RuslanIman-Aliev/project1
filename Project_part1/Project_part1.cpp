ce78ae883e20f8f1e515f719e48bc81185b644bf
Иман-Алиев Руслан <s0970802047@gmail.com>
2023-10-24 23:05:56 +0300

    Project part 2

--- Project_part1/Project_part1.cpp
+++ Project_part1/Project_part1.cpp
@@ -5,6 +5,24 @@
 
 using namespace std;
 
+bool check_balanced_parentheses(const string& expression) {
+    int open_brackets_count = 0;
+
+    for (char current_char : expression) {
+        if (current_char == '(') {
+            open_brackets_count++;
+        }
+        else if (current_char == ')') {
+            open_brackets_count--;
+            if (open_brackets_count < 0) {
+                return false;
+            }
+        }
+    }
+
+    return open_brackets_count == 0;
+}
+
 double evaluate_expression(const string& expression, size_t& index) {
     double result = 0;
     char last_operator = '+';
@@ -28,26 +46,47 @@ double evaluate_expression(const string& expression, size_t& index) {
                 char op = expression[index];
 
                 index++;
+                while (index < expression.length() && expression[index] == ' ') {
+                    index++;
+                }
                 if (index >= expression.length()) {
                     throw invalid_argument("Missing number after operator");
                 }
-                double next_number = stod(expression.substr(index));
 
-                while (index < expression.length() && (isdigit(expression[index]) || expression[index] == ' ')) {
+                if (expression[index] == '(') {
                     index++;
-                }
 
-                if (op == '*') {
-                    number *= next_number;
-                }
-                else if (op == '/') {
-                    if (next_number == 0) {
-                        throw runtime_error("Division by zero");
+                    double sub_result = evaluate_expression(expression, index);
+                    index++;
+
+                    if (op == '*') {
+                        number *= sub_result;
                     }
-                    number /= next_number;
+                    else if (op == '/') {
+                        if (sub_result == 0) {
+                            throw runtime_error("Division by zero");
+                        }
+                        number /= sub_result;
+                    }
+                }
+                else {
+                    double next_number = stod(expression.substr(index));
+
+                    while (index < expression.length() && (isdigit(expression[index]) || expression[index] == ' ')) {
+                        index++;
+                    }
+
+                    if (op == '*') {
+                        number *= next_number;
+                    }
+                    else if (op == '/') {
+                        if (next_number == 0) {
+                            throw runtime_error("Division by zero");
+                        }
+                        number /= next_number;
+                    }
                 }
             }
-
             if (last_operator == '+') {
                 result += number;
             }
@@ -73,9 +112,39 @@ double evaluate_expression(const string& expression, size_t& index) {
             }
 
             number_found = true;
+
         }
         else if (current_char == ')') {
             index++;
+            while (index < expression.length() && expression[index] == ' ') {
+                index++;
+            }
+            while (index < expression.length() && (expression[index] == '*' || expression[index] == '/')) {
+                char op = expression[index];
+
+                index++;
+                if (index >= expression.length()) {
+                    throw invalid_argument("Missing number after operator");
+                }
+
+                double next_number = stod(expression.substr(index));
+
+                while (index < expression.length() && (isdigit(expression[index]) || expression[index] == ' ')) {
+                    index++;
+                }
+
+                if (op == '*') {
+                    result *= next_number;
+                }
+                else if (op == '/') {
+                    if (next_number == 0) {
+                        throw runtime_error("Division by zero");
+                    }
+                    result /= next_number;
+                }
+
+            }
+
             break;
         }
         else {
@@ -101,7 +170,10 @@ void function_for_read_data() {
         if (input == "q") {
             break;
         }
-
+        if (!check_balanced_parentheses(input)) {
+            cout << "Error: Unbalanced parentheses" << endl;
+            continue;
+        }
         try {
             size_t index = 0;
             double result = evaluate_expression(input, index);
